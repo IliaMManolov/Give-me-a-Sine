fa9a556b3745ada8a9b3667e552d1150137782a6
Iliya Manolov <ilmanfordinner@gmail.com>
2017-01-20 22:47:30 +0200

    Added a wave renderer

---
+++ Give me a Sine/Assets/Scripts/LineDrawScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineDrawScript : MonoBehaviour {
+
+
+    class Segment
+    {
+        public Vector2 relativeStartingPosition;
+        public float timeStamp;     //in seconds
+        public float frequency;
+        Segment(Vector2 a, float b, float c)
+        {
+            relativeStartingPosition = a;
+            timeStamp = b;
+            frequency = c;
+        }
+    };
+
+    LineRenderer waveRenderer;
+    public float movementSpeed;
+
+    void Start ()
+    {
+        waveRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        /*for (int i=0;i<waveRenderer.numPositions;i++)
+        {
+            Vector3 currentPosition = waveRenderer.GetPosition(i);
+            currentPosition.x = currentPosition.x - movementSpeed;
+            waveRenderer.SetPosition(i, currentPosition);
+        }*/
+        //int index = 0;
+	}
+}
---
+++ Give me a Sine/Assets/Scripts/SineRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineRenderer : MonoBehaviour {
+
+    [Range(0.001f, 20.0f)]
+    public float spacing;
+    [Range(0.001f, 50.0f)]
+    public float length;
+    [Range(0.001f, 20.0f)]
+    public float frequency;
+    public float amplitude;
+    public float x;
+    [Range(0.1f, 20.0f)]
+    public float speed;
+    LineRenderer sine;
+
+    public Vector2 positionOffset;
+
+	void Start ()
+    {
+        sine = GetComponent<LineRenderer>();
+
+        renderSine(sine);
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        renderSine(sine);
+	}
+
+    void renderSine(LineRenderer target)
+    {
+        //target.numPositions = 0;
+        Vector3[] positions = new Vector3[(int)(length/(spacing*(1/frequency)))];
+        
+        for(int i=0;i<(int)(length/(spacing * (1/frequency)));i++)
+        {
+            Vector2 currPoint;
+            currPoint = new Vector2((x + i * spacing * (1/frequency) + positionOffset.x),
+                                    (Mathf.Sin(Time.time * speed + x + i * spacing) * amplitude) + positionOffset.y);
+            positions[i]=currPoint;
+        }
+        target.numPositions = positions.Length;
+        target.SetPositions(positions);
+    }
+}
