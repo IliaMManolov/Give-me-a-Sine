9124c6aa05d3df3e822c37b1dce471348563c98b
Iliya Manolov <ilmanfordinner@gmail.com>
2017-01-21 20:21:46 +0200

    Fixed scene...

---
+++ Give me a Sine/Assets/Scripts/PlayerPositioning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositioning : MonoBehaviour {
+
+    UniversalWaveRender renderer;
+	void Start ()
+    {
+        renderer = GameObject.FindGameObjectWithTag("sine").GetComponent<UniversalWaveRender>();
+		
+	}
+	
+	void Update ()
+    {
+        int l = 0, r = renderer.lineRenderer.numPositions;
+
+        while (l<r-1)
+        {
+            int m = (l + r) / 2;
+            if (renderer.lineRenderer.GetPosition(m).x < transform.position.x)
+                l = m;
+            else
+                r = m;
+
+        }
+
+        transform.position = new Vector3(transform.position.x, renderer.lineRenderer.GetPosition(l).y);
+
+	}
+}
